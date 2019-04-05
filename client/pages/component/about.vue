07ec1e6149ec2fd18bc0e3a72400e03f77005a0f
xueenze <kameleon@126.com>
2019-04-05 19:31:37 +0800

    引入colorui，完善底部banner，完善【我】TAB页面内容！

---
+++ client/pages/component/about.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class = "about-wrapper">
+		<view class="cu-timeline">
+			<view class="cu-time">目前</view>
+			<view class="cu-item text-blue">
+				<view class="bg-blue content shadow-blur">
+					就职于【国泰基金管理有限公司】，担任高级前端开发工程师，主要负责公司直销平台以及代销产品的全栈开发
+				</view>
+			</view>
+			<view class="cu-time">2018-06</view>
+			<view class="cu-item">
+				<view class="bg-cyan content shadow-blur">
+					就职于【阅文集团】，担任前端开发工程师，主要负责NOAH活动模板引擎（中台）以及起点读书各类运营H5页面的开发
+				</view>
+			</view>
+			<view class="cu-time">2018-02</view>
+			<view class="cu-item">
+				<view class="content">
+					玩起了【微信小程序】，试图在小程序上做一些工具类的应用
+				</view>
+			</view>
+			<view class="cu-time">2017-05</view>
+			<view class="cu-item">
+				<view class="bg-cyan content shadow-blur">
+					就职于【腾讯科技深圳有限公司】，担任全栈开发工程师，主要负责公司内部站点的开发
+				</view>
+			</view>
+			<view class="cu-time">2016-10</view>
+			<view class="cu-item">
+				<view class="content">
+					玩起了公众号运营，建立了【胖乌鸦的巢】，研究一些公众号运营的方法，会发表一些矫情的文章
+				</view>
+			</view>
+			<view class="cu-time">2015-07</view>
+			<view class="cu-item">
+				<view class="content bg-green shadow-blur">
+					毕业于【四川大学】-软件工程专业
+				</view>
+			</view>
+			<view class="cu-time">2014-12</view>
+			<view class="cu-item">
+				<view class="bg-cyan content shadow-blur">
+					就职于成都【SAP】【实习生】，从事SuccessFactor的开发，主要使用JAVA进行WEB开发
+				</view>
+			</view>
+			<view class="cu-time">2014-03</view>
+			<view class="cu-item">
+				<view class="bg-cyan content shadow-blur">
+					就职于【腾讯科技深圳有限公司】【实习生】，从事内部应用的开发
+				</view>
+			</view>
+			<view class="cu-time">2011-09</view>
+			<view class="cu-item">
+				<view class="content">
+					进入【四川大学】软件工程专业，开始苦逼的程序员大学生活
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.about-wrapper{
+	}
+</style>
---
+++ client/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="index-wrapper">
+		<about v-if="PageCur=='about'"></about>
+		<view class="cu-bar tabbar bg-white shadow foot">
+			<view class="action" @click="NavChange" data-cur="about">
+				<view class='icon-cu-image'>
+					<image :src="'/static/tabbar/about' + [PageCur == 'about' ? '_cur' : ''] + '.png'"></image>
+				</view>
+				<view :class="PageCur == 'about' ? 'text-green' : 'text-gray'">我</view>
+			</view>
+			<view class="action" @click="NavChange" data-cur="basics">
+				<view class='icon-cu-image'>
+					<image :src="'/static/tabbar/basics' + [PageCur == 'basics' ? '_cur' : ''] + '.png'"></image>
+				</view>
+				<view :class="PageCur == 'basics' ? 'text-green' : 'text-gray'">作品</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import about from '@/pages/component/about.vue'
+	
+	export default {
+		data() {
+			return {
+				PageCur: 'about'
+			};
+		},
+		components:{
+			about
+		},
+		methods: {
+			NavChange: function(e) {
+				this.PageCur = e.currentTarget.dataset.cur;
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.index-wrapper{
+		padding-bottom: 100upx;
+	}
+</style>
