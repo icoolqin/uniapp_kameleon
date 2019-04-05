fc50fd0eb3be85c109a11fb424333df52dae38c0
xueenze <kameleon@126.com>
2019-04-05 19:39:12 +0800

    完善【我】页面的内容！

--- client/pages/component/about.vue
+++ client/pages/component/about.vue
@@ -1,34 +1,28 @@
 <template>
 	<view class = "about-wrapper">
 		<view class="cu-timeline">
-			<view class="cu-time">目前</view>
+			<view class="cu-time">2018-06</view>
 			<view class="cu-item text-blue">
 				<view class="bg-blue content shadow-blur">
 					就职于【国泰基金管理有限公司】，担任高级前端开发工程师，主要负责公司直销平台以及代销产品的全栈开发
 				</view>
 			</view>
-			<view class="cu-time">2018-06</view>
-			<view class="cu-item">
-				<view class="bg-cyan content shadow-blur">
-					就职于【阅文集团】，担任前端开发工程师，主要负责NOAH活动模板引擎（中台）以及起点读书各类运营H5页面的开发
-				</view>
-			</view>
-			<view class="cu-time">2018-02</view>
-			<view class="cu-item">
-				<view class="content">
-					玩起了【微信小程序】，试图在小程序上做一些工具类的应用
-				</view>
-			</view>
 			<view class="cu-time">2017-05</view>
 			<view class="cu-item">
 				<view class="bg-cyan content shadow-blur">
-					就职于【腾讯科技深圳有限公司】，担任全栈开发工程师，主要负责公司内部站点的开发
+					就职于【阅文集团】，担任前端开发工程师，主要负责NOAH活动模板引擎（中台）以及起点读书各类运营H5页面的开发
 				</view>
 			</view>
 			<view class="cu-time">2016-10</view>
 			<view class="cu-item">
 				<view class="content">
 					玩起了公众号运营，建立了【胖乌鸦的巢】，研究一些公众号运营的方法，会发表一些矫情的文章
+				</view>
+			</view>
+			<view class="cu-time">2015-07</view>
+			<view class="cu-item">
+				<view class="bg-cyan content shadow-blur">
+					就职于【腾讯科技深圳有限公司】，担任全栈开发工程师，主要负责公司内部站点的开发
 				</view>
 			</view>
 			<view class="cu-time">2015-07</view>
